3970af23e0c2a9a7868f881fcb7185de9c885f32
Navyabayavarapu <navyabayavarapu@gmail.com>
2026-06-23 21:52:42 +0530

    Add files via upload

---
+++ DeepSkilling/Module 1 - Design Patterns and Principles/MVCPattern/StudentView.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace MVCPattern
+{
+    public class StudentView
+    {
+        public void DisplayStudentDetails(Student student)
+        {
+            Console.WriteLine("Student Details:");
+            Console.WriteLine($"ID: {student.Id}");
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Grade: {student.Grade}");
+        }
+    }
+}
