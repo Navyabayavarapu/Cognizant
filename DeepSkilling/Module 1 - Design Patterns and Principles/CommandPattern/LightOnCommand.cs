b9b772f95dc65c8add611dd77b31e0ea3af59e4e
Navyabayavarapu <navyabayavarapu@gmail.com>
2026-06-23 21:39:09 +0530

    Add files via upload

---
+++ DeepSkilling/Module 1 - Design Patterns and Principles/CommandPattern/LightOnCommand.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CommandPattern
+{
+    public class LightOnCommand : ICommand
+    {
+        private Light _light;
+
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.TurnOn();
+        }
+    }
+}
